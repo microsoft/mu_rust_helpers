77bc76ff77a6c14919e8589e403f5d5847fda14b
David Lee <83154485+dachihy@users.noreply.github.com>
2024-09-19 12:32:57 -0700

    boot_services.rs: Add a test for allocate pool (#17)
    ## Description
    
    Adds a test for allocate pool wrapper.
    
    - [ ] Impacts functionality?
    - [ ] Impacts security?
    - [ ] Breaking change?
    - [x] Includes tests?
    - [ ] Includes documentation?
    
    ## How This Was Tested
    
    Run Test with PASS result
    
    ## Integration Instructions
    
    N/A

--- boot_services/src/boot_services.rs
+++ boot_services/src/boot_services.rs
@@ -1487,4 +1487,24 @@ mod test {
         let status = boot_services.free_pages(0x100000, 10);
         assert!(matches!(status, Ok(())));
     }
+
+    #[test]
+    fn test_allocate_pool() {
+        let boot_services = boot_services!(allocate_pool = efi_allocate_pool);
+
+        extern "efiapi" fn efi_allocate_pool(
+            mem_type: efi::MemoryType,
+            size: usize,
+            buffer: *mut *mut c_void,
+        ) -> efi::Status {
+            let expected_mem_type: efi::MemoryType = MemoryType::MEMORY_MAPPED_IO.into();
+            assert_eq!(mem_type, expected_mem_type);
+            assert_eq!(size, 10);
+            unsafe { ptr::write(buffer, 0x55AA as *mut c_void) };
+            efi::Status::SUCCESS
+        }
+
+        let status = boot_services.allocate_pool(MemoryType::MEMORY_MAPPED_IO, 10);
+        assert_eq!(status, Ok(0x55AA as *mut u8));
+    }
 }
