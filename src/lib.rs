38bc2368c0ac59e1a80e251699a112b8de9ed867
Edwin Zhang <edwinz@microsoft.com>
2024-09-19 11:37:08 -0700

    Personal/edwinz/runtime services modifications (#6)
    * Expose runtime_services API
    
    * Remove mutable requirement for set_variable input

--- src/lib.rs
+++ src/lib.rs
@@ -8,5 +8,8 @@ pub use boot_services;
 #[cfg(feature = "guid")]
 pub use guid;
 
+#[cfg(feature = "runtime_services")]
+pub use runtime_services;
+
 #[cfg(feature = "tpl_mutex")]
 pub use tpl_mutex;
