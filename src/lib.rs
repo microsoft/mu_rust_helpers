17e875318e5ae3cb5a21648b0b1a34f275bbabb0
Daniel Kouchekinia <kouchekiniad@microsoft.com>
2024-09-19 18:47:29 -0700

    Fixed additional merge conflict errors.

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,9 @@ extern crate alloc;
 #[cfg(feature = "boot_services")]
 pub use boot_services;
 
+#[cfg(feature = "runtime_services")]
+pub use runtime_services;
+
 #[cfg(feature = "guid")]
 pub use guid;
 
