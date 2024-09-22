737f02df70f38c718c99e189a8eff19ba409340b
Daniel Kouchekinia <kouchekiniad@microsoft.com>
2024-09-22 10:38:42 -0700

    Removed duplicate export of runtime_services in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -11,8 +11,5 @@ pub use runtime_services;
 #[cfg(feature = "guid")]
 pub use guid;
 
-#[cfg(feature = "runtime_services")]
-pub use runtime_services;
-
 #[cfg(feature = "tpl_mutex")]
 pub use tpl_mutex;
