b1c368668edbcc890704d8c943da3d4e8ec01068
Michael Kubacki <michael.kubacki@microsoft.com>
2025-02-11 20:08:20 -0500

    Remove boot_services and tpl_mutex crates
    These have been moved to another repo that will be published soon.
    
    To prevent duplicate code and reduce dependencies on the code in
    this repo, the code is removed.
    
    Signed-off-by: Michael Kubacki <michael.kubacki@microsoft.com>

--- src/lib.rs
+++ src/lib.rs
@@ -4,17 +4,11 @@ extern crate alloc;
 
 pub mod macros;
 
-#[cfg(feature = "boot_services")]
-pub use boot_services;
-
 #[cfg(feature = "runtime_services")]
 pub use runtime_services;
 
 #[cfg(feature = "guid")]
 pub use guid;
-
-#[cfg(feature = "tpl_mutex")]
-pub use tpl_mutex;
 
 #[cfg(feature = "uefi_decompress")]
 pub use uefi_decompress;
