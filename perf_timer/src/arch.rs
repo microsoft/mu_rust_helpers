4837cce54c112ca2429ebc06741d7f5be418d701
sherry fan <60113548+berlin-with0ut-return@users.noreply.github.com>
2025-03-14 13:40:19 -0700

    Comprehensive timer fix for QEMU (#52)
    ## Description
    
    Fix divide by zero error in timer for QEMU and improve frequency
    calculation overall. The new approach attempts to use CPUID leaf 0x16
    (for nominal frequency on QEMU), as well as CPUID leaf 0x15 (for TSC
    frequency on physical platforms), and defaults to the ACPI frequency if
    both CPUID leaves fail (same behavior as mu_basecore).
    
    I also added an ability to cache the frequency value since it should not
    change during runtime for more consistent perf results.
    
    Leaving as draft for now because I'm unsure if this is the best strategy
    for timing.
    
    - [X] Impacts functionality?
    - [ ] Impacts security?
    - [ ] Breaking change?
    - [ ] Includes tests?
    - [ ] Includes documentation?
    
    ## How This Was Tested
    
    I tested all 3 cases (Skylake QEMU, physical platform, qemu64) and all
    give reasonable results without crashing.
    
    ## Integration Instructions
    
    N/A. Although it should be noted for perf, we would generally prefer
    using a realistic cpu model or a physical platform over `qemu64`.

--- perf_timer/src/arch.rs
+++ perf_timer/src/arch.rs
@@ -1,4 +1,4 @@
-use core::u64;
+use core::{sync::atomic::AtomicU64, u64};
 
 #[cfg(target_arch = "x86_64")]
 pub use x64::X64 as Arch;
@@ -6,11 +6,16 @@ pub use x64::X64 as Arch;
 #[cfg(target_arch = "aarch64")]
 pub use aarch64::Aarch64 as Arch;
 
+// QEMU uses the ACPI frequency when CPUID-based frequency determination is not available.
+const DEFAULT_ACPI_TIMER_FREQUENCY: u64 = 3579545;
+
+static PERF_FREQUENCY: AtomicU64 = AtomicU64::new(0);
+
 pub trait ArchFunctionality {
     /// Value of the counter.
     fn cpu_count() -> u64;
     /// Value in Hz of how often the counter increment.
-    fn cpu_count_frequency() -> u64;
+    fn perf_frequency() -> u64;
     /// Value the performance counter starts with when it rolls over.
     fn cpu_count_start() -> u64 {
         0
@@ -24,7 +29,10 @@ pub trait ArchFunctionality {
 #[cfg(target_arch = "x86_64")]
 pub(crate) mod x64 {
     use super::*;
-    use core::arch::x86_64::{self, CpuidResult};
+    use core::{
+        arch::x86_64::{self, CpuidResult},
+        sync::atomic::Ordering,
+    };
 
     pub struct X64;
     impl ArchFunctionality for X64 {
@@ -43,8 +51,19 @@ pub(crate) mod x64 {
             unsafe { x86_64::_rdtsc() }
         }
 
-        fn cpu_count_frequency() -> u64 {
-            // https://en.wikipedia.org/wiki/CPUID
+        fn perf_frequency() -> u64 {
+            let cached = PERF_FREQUENCY.load(Ordering::Relaxed);
+            if cached != 0 {
+                return cached;
+            }
+
+            let hypervisor_leaf = unsafe { x86_64::__cpuid(0x1) };
+            let is_vm = (hypervisor_leaf.ecx & (1 << 31)) != 0;
+
+            if is_vm {
+                log::warn!("Running in a VM - CPUID-based frequency may not be reliable.");
+            }
+
             let CpuidResult {
                 eax, // Ratio of TSC frequency to Core Crystal Clock frequency, denominator.
                 ebx, // Ratio of TSC frequency to Core Crystal Clock frequency, numerator.
@@ -52,12 +71,29 @@ pub(crate) mod x64 {
                 ..
             } = unsafe { x86_64::__cpuid(0x15) };
 
-            #[cfg(feature = "validate_cpu_features")]
-            if ecx == 0 {
-                panic!("CPU does not support CPUID-based frequency determination");
+            // If not a VM, attempt to use CPUID leaf 0x15
+            if !is_vm && ecx != 0 && eax != 0 && ebx != 0 {
+                let frequency = (ecx as u64 * ebx as u64) / eax as u64;
+                PERF_FREQUENCY.store(frequency, Ordering::Relaxed);
+                log::trace!("Used CPUID leaf 0x15 to determine CPU frequency: {}", frequency);
+                return frequency;
             }
 
-            (ecx * (ebx / eax)) as u64
+            // If VM or CPUID 0x15 fails, attempt to use CPUID 0x16
+            // Based on testing in QEMU, leaf 0x16 is generally more reliable on VMs
+            let CpuidResult { eax, .. } = unsafe { x86_64::__cpuid(0x16) };
+            if eax != 0 {
+                // Leaf 0x16 gives the frequency in MHz.
+                let frequency = (eax * 1_000_000) as u64;
+                PERF_FREQUENCY.store(frequency, Ordering::Relaxed);
+                log::trace!("Used CPUID leaf 0x16 to determine CPU frequency: {}", frequency);
+                return frequency;
+            }
+
+            log::warn!("Unable to determine CPU frequency using CPUID leaves, using default ACPI timer frequency");
+
+            PERF_FREQUENCY.store(DEFAULT_ACPI_TIMER_FREQUENCY, Ordering::Relaxed);
+            DEFAULT_ACPI_TIMER_FREQUENCY
         }
     }
 }
